8ec614aafca4e323837546184d46010f8ad4d8a7
mine <siutszfung21@gmail.com>
2025-11-18 09:44:50 +0900

    更新

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@ void main() {
 	const char* Participate[10] = {
 		"GP52A186-10-林厚廷",
 		"GP52A186-01-川口透和",
-		"GP52A186-02-蕭梓峰"
+		"GP52A186-03-蕭梓峰"
 		"GP52A186-05-ZLATOPOLSKII ALEKSANDR",
 		"GP52A186-06-トウコウ",
 		"GP52A186-08-賴昱宏"
