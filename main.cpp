0d2341ce5d754b0d1e445f8f1b18687e1a388005
mine <siutszfung21@gmail.com>
2025-11-18 09:33:54 +0900

    追加

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@ void main() {
 	const char* Participate[10] = {
 		"GP52A186-10-林厚廷",
 		"GP52A186-01-川口透和",
+		"GP52A186-02-蕭梓峰"
 		"GP52A186-05-ZLATOPOLSKII ALEKSANDR",
 		"GP52A186-06-トウコウ",
-		"GP52A186-08-ｿ牀Rｧｻ"
+		"GP52A186-08-賴昱宏"
 	};
 }
